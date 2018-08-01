0a736c75f17476e3fce6d60ef6cee8624b15fe50
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-01 14:07:09 -0400

    Initial commit

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+
+        }
+    }
+}
