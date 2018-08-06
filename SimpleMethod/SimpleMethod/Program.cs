791a6b6f2d4303fe02deb0c20d117fe32f7536b2
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-06 13:25:37 -0400

    Creating a simple static void method

---
+++ SimpleMethod/SimpleMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            Console.WriteLine();
+        }
+
+        //helper method
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
