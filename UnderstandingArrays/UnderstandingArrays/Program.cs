ddb51dc632690e31738839ede98dc4afa87733d8
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-06 11:43:38 -0400

    Understanding arrays

---
+++ UnderstandingArrays/UnderstandingArrays/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring integer arrays
+            /*int[] numbers = new int[5];
+
+            numbers[0] = 4;
+            numbers[1] = 8;
+            numbers[2] = 15;
+            numbers[3] = 16;
+            numbers[4] = 23;
+
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers.Length);
+            Console.ReadLine();
+            */
+
+            //Declaring string arrays
+            /*
+            int[] numbers = new int[] { 4, 8, 15, 16, 23, 42 };
+
+            string[] names = new string[] { "Eddie", "Alex", "Michael" , "David Lee" };
+            //looping through the array using for loops
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadLine();
+            */
+
+            /*
+            //looping through the array using foreach
+            string[] names = new string[] { "Eddie", "Alex", "Michael", "David Lee" };
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+            */
+
+            
+
+
+
+        }
+    }
+}
