09863e1dd85de93b27dddb85657153864554cd15
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-15 12:31:30 -0400

    Working with date and time

---
+++ WorkingWithDatesAndTimes/WorkingWithDatesAndTimes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithDatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue.ToString());
+            //Console.WriteLine(myValue.ToShortDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+            //Console.WriteLine(myValue.ToLongDateString());
+            Console.WriteLine(myValue.ToLongTimeString());
+            Console.Read();
+        }
+    }
+}
