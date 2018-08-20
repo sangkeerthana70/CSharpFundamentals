3ed057a17a4a6db795e2e3eda325f9f77905c7b6
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-20 17:23:32 -0400

    Lifetime of Objects

---
+++ ObjectLifetime/ObjectLifetime/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLifetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "OldsMobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+
+            Car myOtherCar;
+            myOtherCar = myCar;//myOtherCar is a variable that refers to the same memory address as myCar.
+
+            Console.WriteLine("variable myOtherCar points to same memory as myCar: {0} {1} {2} {3}",
+                myOtherCar.Make,
+                myOtherCar.Model,
+                myOtherCar.Year,
+                myOtherCar.Color);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+    }
+}
