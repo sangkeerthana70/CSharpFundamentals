33d0b27bf371f28633a4bae8d6610fa42bb693b8
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-25 07:32:53 -0400

    Scope of a variable

---
+++ ScopeAndAccesibilityModifiers/ScopeAndAccesibilityModifiers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScopeAndAccesibilityModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string j = "";
+            for (int i = 0; i < 10; i++)
+            {
+                j = i.ToString();
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Outside of the for: " + j);
+            Console.ReadLine();
+        }
+    }
+}
