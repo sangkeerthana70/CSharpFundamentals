3643b64c3bbb588dca443bca0c3187d35b0e7b87
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-25 08:19:12 -0400

    Accesibility of private and public classes

--- ScopeAndAccesibilityModifiers/ScopeAndAccesibilityModifiers/Program.cs
+++ ScopeAndAccesibilityModifiers/ScopeAndAccesibilityModifiers/Program.cs
@@ -8,16 +8,53 @@ namespace ScopeAndAccesibilityModifiers
 {
     class Program
     {
+        private static string k = "";
+
         static void Main(string[] args)
         {
             string j = "";
+
             for (int i = 0; i < 10; i++)
             {
                 j = i.ToString();
+                k = i.ToString();
                 Console.WriteLine(i);
+
+                if(i == 9)
+                {
+                    string l = i.ToString();
+                }
+                //Console.WriteLine(l); l is not accessible out of if block
             }
-            Console.WriteLine("Outside of the for: " + j);
+            //Console.WriteLine(i);
+            Console.WriteLine("j is available outside of the for: " + j);
+            Console.WriteLine("K is available out of the for loop: " + k);
+
+            HelperMethod();
+
+            Car myCar = new Car();
+            myCar.DoSomething();
+
             Console.ReadLine();
         }
+
+        //helper method
+        static void HelperMethod()
+        {
+            Console.WriteLine("Value of k in for loop is accessible in helper method: " + k);
+        }
     }
+
+    class Car
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine(helperMethod());
+        }
+        private string helperMethod()
+        {
+            return "Hello World";
+        }
+    }
+
 }
