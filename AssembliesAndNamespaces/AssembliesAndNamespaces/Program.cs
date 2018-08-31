fadf8122966dfe0d895d7be9f0225b74eb7582ab
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-31 14:29:29 -0400

    Different methods in the class library under system namespace

--- AssembliesAndNamespaces/AssembliesAndNamespaces/Program.cs
+++ AssembliesAndNamespaces/AssembliesAndNamespaces/Program.cs
@@ -12,7 +12,7 @@ namespace AssembliesAndNamespaces
             string[] lines = { "First line", "Second line", "Third line" };
             // WriteAllLines creates a file, writes a collection of strings to the file,
             // and then closes the file.  You do NOT need to call Flush() or Close().
-            System.IO.File.WriteAllLines(@"C: \Users\anuradha\Desktop\Selenium\C#Lesson17MicrosoftVirtualAcademy\WriteLinestoText.txt", lines);
+            System.IO.File.WriteAllLines(@"C: \Users\anuradha\Desktop\Selenium\C#Lesson17\WriteLinestoText.txt", lines);
 
 
             // Example #2: Write one string to a text file.
@@ -20,8 +20,26 @@ namespace AssembliesAndNamespaces
                            "a class defines the data and behavior of the data type. ";
             // WriteAllText creates a file, writes the specified string to the file,
             // and then closes the file.    You do NOT need to call Flush() or Close().
-            File.WriteAllText(@"C: \Users\anuradha\Desktop\Selenium\C#Lesson17MicrosoftVirtualAcademy\WriteText.txt", text);
+            File.WriteAllText(@"C: \Users\anuradha\Desktop\Selenium\C#Lesson17\WriteText.txt", text);
 
+
+            // Example #3: Write only some strings in an array to a file.
+            // The using statement automatically flushes AND CLOSES the stream and calls 
+            // IDisposable.Dispose on the stream object.
+            // NOTE: do not use FileStream for text files because it writes bytes, but StreamWriter
+            // encodes the output as text.
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(@"C: \Users\anuradha\Desktop\Selenium\C#Lesson17\WriteLines2.txt"))
+            {
+                foreach (string line in lines)
+                {
+                    // If the line doesn't contain the word 'Second', write the line to the file.
+                    if (!line.Contains("Second"))
+                    {
+                        file.WriteLine(line);
+                    }
+                }
+            }
 
 
            Console.WriteLine("Hello World");
