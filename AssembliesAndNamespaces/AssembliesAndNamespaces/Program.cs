1589c63028a1169f8d9c39da02487b5a7e0a3ccd
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-31 01:49:00 -0400

    Initial commit

---
+++ AssembliesAndNamespaces/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+            System.Console.ReadLine();
+
+        }
+    }
+}
