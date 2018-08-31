90f649f412b743090fee732e3bf1c2a6644e28d7
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-08-31 12:18:53 -0400

    Writing data to a text file using System.IO namespace

--- AssembliesAndNamespaces/AssembliesAndNamespaces/Program.cs
+++ AssembliesAndNamespaces/AssembliesAndNamespaces/Program.cs
@@ -1,4 +1,5 @@
-﻿//using System;
+﻿using System;
+using System.IO;
 
 namespace AssembliesAndNamespaces
 {
@@ -6,17 +7,26 @@ namespace AssembliesAndNamespaces
     {
         static void Main(string[] args)
         {
+            // Example #1: Write an array of strings to a file.
+            // Create a string array that consists of three lines.
+            string[] lines = { "First line", "Second line", "Third line" };
+            // WriteAllLines creates a file, writes a collection of strings to the file,
+            // and then closes the file.  You do NOT need to call Flush() or Close().
+            System.IO.File.WriteAllLines(@"C: \Users\anuradha\Desktop\Selenium\C#Lesson17MicrosoftVirtualAcademy\WriteLinestoText.txt", lines);
+
+
             // Example #2: Write one string to a text file.
             string text = "A class is the most powerful data type in C#. Like a structure, " +
                            "a class defines the data and behavior of the data type. ";
             // WriteAllText creates a file, writes the specified string to the file,
             // and then closes the file.    You do NOT need to call Flush() or Close().
-            System.IO.File.WriteAllText(@"C: \Users\anuradha\Desktop\Selenium\C#Lesson17MicrosoftVirtualAcademy\WriteText.txt", text);
+            File.WriteAllText(@"C: \Users\anuradha\Desktop\Selenium\C#Lesson17MicrosoftVirtualAcademy\WriteText.txt", text);
 
 
 
-            System.Console.WriteLine("Hello World");
-            System.Console.ReadLine();
+           Console.WriteLine("Hello World");
+           Console.WriteLine("Using System and System.IO classes from the same namespaces");
+           Console.ReadLine();
 
         }
     }
