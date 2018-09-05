1e79377c6bbcbbe1cc3c32020539c9c13d6b636f
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-09-05 14:37:30 -0400

    Creating a Class library and adding references

---
+++ MyCodeLibrary/MyCodeLibrary/Scrape.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCodeLibrary
+{
+    public class Scrape
+    {
+        public string ScrapeWebPage(string url)
+        {
+            WebClient Client = new WebClient();
+            return Client.DownloadString(url);
+
+        }
+
+        public string ScrapeWebPage(string url, string filepath)
+        {
+            WebClient Client = new WebClient();
+            string reply = Client.DownloadString(url);
+            
+            File.WriteAllText(filepath, reply);
+            return reply;
+        }
+    }
+}
