5c682d477e332e4cba28cc66e2f52645af4bbac5
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-09-05 13:22:50 -0400

    Different methods in List namespace

--- WorkingWIthLists/WorkingWIthLists/Program.cs
+++ WorkingWIthLists/WorkingWIthLists/Program.cs
@@ -10,11 +10,13 @@ namespace WorkingWIthLists
     {
         static void Main(string[] args)
         {
-            List<string> dinosaurs = new List<string>();
+
 
             //The following example demonstrates the default constructor of the List<T> generic class.
             //The default constructor creates a list with the default capacity, 
             //as demonstrated by displaying the Capacity property.
+
+            List<string> dinosaurs = new List<string>();
 
             Console.WriteLine("\nCapacity: {0}", dinosaurs.Capacity);
             
@@ -33,6 +35,31 @@ namespace WorkingWIthLists
 
             Console.WriteLine("\nCapacity: {0}", dinosaurs.Capacity);
             Console.WriteLine("Count: {0}", dinosaurs.Count);
+            //check if the list contains a value that we are looking for and returns a bool
+            Console.WriteLine("\nContains(\"Deinonychus\"): {0}", dinosaurs.Contains("Deinonychus"));
+
+            //insert a value
+            Console.WriteLine("\nInsert(2, \"Compsognathus\")");
+            dinosaurs.Insert(2, "Compsognathus");
+
+            Console.WriteLine();
+            foreach (string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
+
+            // Shows accessing the list using the Item or index value property.
+            Console.WriteLine("\ndinosaurs[3]: {0}", dinosaurs[3]);
+
+            //remove an item from the list
+            Console.WriteLine("\nRemove(\"Compsognathus\")");
+            dinosaurs.Remove("Compsognathus");
+
+            Console.WriteLine();
+            foreach(string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
         }
     }
 }
