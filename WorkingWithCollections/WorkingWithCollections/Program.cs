605d18cdc7c2a1b9f5c9b78f710b564f56c48916
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-09-07 15:37:39 -0400

    Working with collections

---
+++ WorkingWithCollections/WorkingWithCollections/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Make = "OldsMobile";
+            car1.Model = "Cutlas Supreme";
+
+            Car car2 = new Car();
+            car2.Make = "Geo";
+            car2.Model = "Prism";
+
+            Book b1 = new Book();
+            b1.Author = "AnuRadha";
+
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+    }
+}
