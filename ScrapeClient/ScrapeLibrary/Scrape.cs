0241b43978c7690c6762b641cdcae9cfeb0fe07e
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-09-07 12:04:03 -0400

    Create class library and console app inside one project and reference it

---
+++ ScrapeClient/ScrapeLibrary/Scrape.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapeLibrary
+{
+    public class Scrape
+    {
+
+
+        public string ScrapeWebPage(string url)
+        {
+            return GetWebpage(url);
+        }
+
+        public string ScrapeWebPage(string url, string filepath)
+        {
+            string reply = GetWebpage(url);
+            File.WriteAllText(filepath, reply);
+            return reply;
+        }
+
+        //private helper method which gets the web page.
+        private string GetWebpage(string url)
+        {
+            WebClient Client = new WebClient();
+            return Client.DownloadString(url);
+        }
+    }
+
+}
